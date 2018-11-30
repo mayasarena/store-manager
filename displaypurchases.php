c2252092eceee786bd3193203065ec9c32788d9f
Maya Murad <mmurad7@uwo.ca>
2018-11-30 04:43:07 +0000

    Parts 1,2,3,4,6,7,8,9 work

---
+++ displaypurchases.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assignment3 - display product</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
+</head>
+<body>
+
+<?php include "connectdb.php";
+?>
+<h1>Purchase Info</h1>
+<?php include "getpurchaseinfo.php";?>
+<hr>
+<img src="http://www.csd.uwo.ca/~lreid/blendedcs3319/flippedclassroom/four/kids.png" width="216" height="260">
+</body>
+</html>
